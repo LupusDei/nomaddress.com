67cfed36e2d44f1a64a57c9bd5f3f76ba86a0716
Justin Martin <lupusdei108@gmail.com>
2011-11-12 11:18:31 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NomaddressCom::Application.config.secret_token = '39a6660363253dc6b33aad849f408736a715c545ef3fab99a057a97c4c9e9ca7206ac4788c3435a91efed7c74f1882a6b6d2f73acebf2c878bba6257d48cf9fd'
