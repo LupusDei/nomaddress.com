880d4d5987e07f09bdf049838662218011aea725
Justin Martin <lupusdei108@gmail.com>
2011-11-12 22:00:24 -0600

    Added pg to the bundle and replaced sqlite

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,15 +16,6 @@ NomaddressCom::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain
-  :authentication
-  :user_name
-  :password
-  :enable_starttls_auto => true
-}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
