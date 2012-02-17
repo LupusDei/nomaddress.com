176dbe7d6435cc1f7d4ba921237000823ed2728a
Justin Martin <lupusdei108@gmail.com>
2012-02-16 21:59:27 -0600

    fixed session store nonsense

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-NomaddressCom::Application.config.session_store :cookie_store, key: '_nomaddress.com_session'
+NomaddressCom::Application.config.session_store :cookie_store, :key => '_nomaddress.com_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
