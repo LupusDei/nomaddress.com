8fd38d05401d881e10ab20ddd19648f45909f833
Justin Martin <lupusdei108@gmail.com>
2012-02-23 18:11:43 -0600

    Added validation to Address to guarantee a correct address category.  Also added a before validation to set the category to 'Home' if not already defined

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -18,4 +18,18 @@ describe Address do
     adr = Address.find(@valid_adr.id)
     adr.user.first_name.should == "Justin" 
   end
+
+  it "has a default category of 'Home'" do
+    @valid_adr.category.should == "Home"
+  end
+
+  it "must have one of the defined categories" do
+    @valid_adr.category = "crazy invalid"
+    @valid_adr.should_not be_valid
+
+    Address.valid_categories.each do |cat|
+      @valid_adr.category = cat
+      @valid_adr.should be_valid
+    end
+  end
 end
